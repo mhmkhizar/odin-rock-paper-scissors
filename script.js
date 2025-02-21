be7b016e14bb114ea52abc3cf98edee691593cb3
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-02-21 10:56:51 +0500

    Add a blank line for improved readability in getComputerChoice function

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ getComputerChoice();
 function getComputerChoice() {
   // Get random number between 1 to 3
   const randomNumber = Math.floor(Math.random() * 3) + 1;
+
   // Return a choice on the bases of the generated number
   if (randomNumber === 1) {
     return console.log("rock");
