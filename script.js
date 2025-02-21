c6167424f46aac27b6adc04af74c1a44650afe88
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-02-21 10:56:14 +0500

    Fix comment wording in getComputerChoice function for clarity

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ getComputerChoice();
 function getComputerChoice() {
   // Get random number between 1 to 3
   const randomNumber = Math.floor(Math.random() * 3) + 1;
-  // Return each choice on the bases of the generated number
+  // Return a choice on the bases of the generated number
   if (randomNumber === 1) {
     return console.log("rock");
   } else if (randomNumber === 2) {
