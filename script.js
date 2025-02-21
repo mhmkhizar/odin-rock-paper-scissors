a887cbbfa050da71dba18be2723f53fd26b466fc
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-02-21 10:59:02 +0500

    Fix comment casing in getComputerChoice function for consistency

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 getComputerChoice();
 
 function getComputerChoice() {
-  // Get random number between 1 to 3
+  // get random number between 1 to 3
   const randomNumber = Math.floor(Math.random() * 3) + 1;
 
-  // Return a choice on the bases of the generated number
+  // return a choice on the bases of the generated number
   if (randomNumber === 1) {
     return console.log("rock");
   } else if (randomNumber === 2) {
