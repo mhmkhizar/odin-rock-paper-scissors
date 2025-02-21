d6f93112fb3e98b5b32b19be6cd7f84e66a86698
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-02-21 10:55:09 +0500

    Implement getComputerChoice function to randomly select Rock, Paper, or Scissors

---
+++ script.js
@@ -0,0 +1,14 @@
+getComputerChoice();
+
+function getComputerChoice() {
+  // Get random number between 1 to 3
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  // Return each choice on the bases of the number
+  if (randomNumber === 1) {
+    return console.log("rock");
+  } else if (randomNumber === 2) {
+    return console.log("paper");
+  } else {
+    return console.log("scissors");
+  }
+}
