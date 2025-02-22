49c96154d55013d189ae5f2eac6fb6ed1e54a605
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-02-22 14:18:56 +0500

    Implement game loop and scoring for Rock, Paper, Scissors

--- script.js
+++ script.js
@@ -11,14 +11,46 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  const humanChoice = prompt("Rock or Paper or Scissors?").toLowerCase();
-  if (
-    humanChoice === "rock" ||
-    humanChoice === "paper" ||
-    humanChoice === "scissors"
-  ) {
-    return humanChoice;
-  } else {
-    return "Please choose only Rock, Paper, or Scissors (spelling matters, but case doesn't). Try again.";
+  const humanChoice = prompt("Choose Rock, Paper or Scissors?");
+  return humanChoice.toLowerCase();
+}
+
+playGame();
+
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+  }
+
+  function playRound(humanChoice, computerChoice) {
+    if (
+      !(
+        humanChoice === "rock" ||
+        humanChoice === "paper" ||
+        humanChoice === "scissors"
+      )
+    ) {
+      console.log(
+        "Please choose only Rock, Paper, or Scissors (spelling matters, but case doesn't). Try again."
+      );
+    } else if (humanChoice === computerChoice) {
+      console.log("Uh-Oh! It's a tie.");
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      humanScore++;
+      console.log(`You Won! ${humanChoice} beats ${computerChoice}.`);
+    } else {
+      computerScore++;
+      console.log(`You Lose! ${computerChoice} beats ${humanChoice}.`);
+    }
   }
 }
