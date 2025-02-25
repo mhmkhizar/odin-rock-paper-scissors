cf29e2121df4f0a8cc26ddf116889e44fcf62b60
Muhammad Khizar <90056057+mhmkhizar@users.noreply.github.com>
2025-02-25 10:26:21 +0500

    Capitalize choices and update score tracking in playRound function

--- script.js
+++ script.js
@@ -42,22 +42,32 @@ function playRound(humanChoice, computerChoice) {
       "Please choose only Rock, Paper, or Scissors (spelling matters, but case doesn't). Try again."
     );
   } else if (humanChoice === computerChoice) {
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+    computerChoice =
+      computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
+    humanScore++;
+    console.log(`You Chose: ${humanChoice}`);
+    console.log(`Computer Chose: ${computerChoice}`);
     console.log("Uh-Oh! It's a draw.");
   } else if (
     (humanChoice === "rock" && computerChoice === "scissors") ||
     (humanChoice === "paper" && computerChoice === "rock") ||
     (humanChoice === "scissors" && computerChoice === "paper")
   ) {
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+    computerChoice =
+      computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
     humanScore++;
     console.log(`You Chose: ${humanChoice}`);
     console.log(`Computer Chose: ${computerChoice}`);
     console.log(`You Won! ${humanChoice} beats ${computerChoice}`);
   } else {
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1);
+    computerChoice =
+      computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1);
     computerScore++;
     console.log(`You Chose: ${humanChoice}`);
     console.log(`Computer Chose: ${computerChoice}`);
     console.log(`You Lose! ${computerChoice} beats ${humanChoice}`);
   }
 }
-
-playRound(humanSelection, computerSelection);
